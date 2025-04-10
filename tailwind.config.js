6dc40d7bea8a8d3ee35d5636f52cd73a88f8267b
Cristina Gil <crisgilalcon@gmail.com>
2025-04-10 18:18:20 +0200

    Change colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,16 @@
-
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
-        teal: '#3BACB6',
-        darkTeal: '#2A8D96',
-        lightTeal: '#82CCD2',
-        mintGreen: '#E8F4F5',
+        teal: '#8ea6aa',
+        darkTeal: '#6f7970',
+        lightTeal: '#adbbbc',
+        mintGreen: '#d3e2df',
         darkSage: '#2C3E50',
-        sand: '#F4D19B',
-        offWhite: '#F5F5F5',
-        gold: '#D4AF37'
+        sand: '#e5ceac',
+        offWhite: '#e8e7e5',
+        gold: '#e2ad61'
       },
     },
   },
